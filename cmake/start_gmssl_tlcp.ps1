e5df345504bbfc823237c8659f0d86863e32a2ea
Simon <zhaopku09@gmail.com>
2024-06-15 13:38:26 +0800

    Update start_gmssl_tlcp.ps1

--- cmake/start_gmssl_tlcp.ps1
+++ cmake/start_gmssl_tlcp.ps1
@@ -1,12 +1,13 @@
 param (
     [string]$gmsslPath = " D:/a/GmSSL/GmSSL/build/bin/gmssl.exe",
-    [string]$arguments = "tlcp_server -port 4433 -cert D:/a/GmSSL/GmSSL/build/tlcp_server_certs.pem -key D:/a/GmSSL/GmSSL/build/signkey.pem -pass P@ssw0rd -ex_key D:/a/GmSSL/GmSSL/build/enckey.pem -ex_pass P@ssw0rd",
+    [string]$arguments = " --help",
+    #[string]$arguments = "tlcp_server -port 4433 -cert D:/a/GmSSL/GmSSL/build/tlcp_server_certs.pem -key D:/a/GmSSL/GmSSL/build/signkey.pem -pass P@ssw0rd -ex_key D:/a/GmSSL/GmSSL/build/enckey.pem -ex_pass P@ssw0rd",
     [string]$logFile = "tlcp_server.log"
 )
 
 # 启动进程并获取进程对象
 $process = Start-Process -FilePath $gmsslPath `
-                         -ArgumentList $arguments `
+                         #-ArgumentList $arguments `
                          -NoNewWindow `
                          -RedirectStandardOutput $logFile `
                          -PassThru
