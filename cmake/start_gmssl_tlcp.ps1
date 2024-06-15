ce1a4c64a2ca4fc3bd6c3aba52bdddffe1a06a5e
Simon <zhaopku09@gmail.com>
2024-06-15 13:43:49 +0800

    Update start_gmssl_tlcp.ps1

--- cmake/start_gmssl_tlcp.ps1
+++ cmake/start_gmssl_tlcp.ps1
@@ -1,15 +1,15 @@
 param (
-    [string]$gmsslPath = " D:/a/GmSSL/GmSSL/build/bin/gmssl.exe",
+    [string]$gmsslPath = "D:/a/GmSSL/GmSSL/build/bin/gmssl.exe",
     [string]$arguments = " --help",
     #[string]$arguments = "tlcp_server -port 4433 -cert D:/a/GmSSL/GmSSL/build/tlcp_server_certs.pem -key D:/a/GmSSL/GmSSL/build/signkey.pem -pass P@ssw0rd -ex_key D:/a/GmSSL/GmSSL/build/enckey.pem -ex_pass P@ssw0rd",
-    [string]$logFile = "tlcp_server.log"
+    #[string]$logFile = "tlcp_server.log"
 )
 
 # 启动进程并获取进程对象
 $process = Start-Process -FilePath $gmsslPath `
                          -ArgumentList $arguments `
                          -NoNewWindow `
-                         -RedirectStandardOutput $logFile `
+                         #-RedirectStandardOutput $logFile `
                          -PassThru
 Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
 exit 0
