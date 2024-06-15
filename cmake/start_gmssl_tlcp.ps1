ff3c639f8be4d7985e52dc55fb74f593d019d071
Simon <zhaopku09@gmail.com>
2024-06-15 10:40:08 +0800

    Update start_gmssl_tlcp.ps1

--- cmake/start_gmssl_tlcp.ps1
+++ cmake/start_gmssl_tlcp.ps1
@@ -5,12 +5,14 @@ $process = Start-Process -FilePath "bin/gmssl.exe" `
                          -PassThru
 
 # 等待进程完成或超时
-$timeout = 2  # 设置超时时间，单位为秒
+$timeout = 10  # 设置超时时间，单位为秒
 $waitResult = $process.WaitForExit($timeout * 1000)  # 将超时时间转换为毫秒
 
 if (!$waitResult) {
     Write-Host "Process did not complete within $timeout seconds. Terminating."
-    $process.Kill()  # 如果超时，可以尝试终止进程
+    $process.Kill()  # 如果超时，终止进程
+    exit 1  # 返回一个非零值，表示超时错误
 } else {
     Write-Host "Process completed successfully."
+    exit 0  # 返回零值，表示成功
 }
