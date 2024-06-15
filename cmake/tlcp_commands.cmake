823283d83fbb893334ccc8d4693b24e13f036145
Simon <zhaopku09@gmail.com>
2024-06-15 10:09:01 +0800

    Update tlcp_commands.cmake

--- cmake/tlcp_commands.cmake
+++ cmake/tlcp_commands.cmake
@@ -15,13 +15,14 @@ if(NOT EXISTS enckey.pem)
 	message(FATAL_ERROR "file does not exist")
 endif()
 
-message(FATAL_ERROR "server STARTED ")
+message(FATAL_ERROR "111server STARTTTING ")
 if(WIN32)
 	execute_process(
 	    COMMAND powershell -ExecutionPolicy Bypass -File "../cmake/start_gmssl_tlcp.ps1"
 	    RESULT_VARIABLE SERVER_RESULT
-	    TIMEOUT 10
+	    TIMEOUT 5
 	)
+message(FATAL_ERROR "222server STARTED ")
 else()
 	execute_process(
 		COMMAND bash -c "sudo nohup bin/gmssl tlcp_server -port 4433 -cert tlcp_server_certs.pem -key signkey.pem -pass P@ssw0rd -ex_key enckey.pem -ex_pass P@ssw0rd > tlcp_server.log 2>&1 &"
@@ -32,7 +33,7 @@ endif()
 if(NOT ${SERVER_RESULT} EQUAL 0)
 	message(FATAL_ERROR "server failed to start")
 endif()
-message(FATAL_ERROR "server STARTED ")
+message(FATAL_ERROR "333server STARTED ")
 execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 2)
 
 if (WIN32)
