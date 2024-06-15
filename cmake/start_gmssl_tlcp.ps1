d0e5a43d9454e8a29ca5134dd0e482bc57b20344
Simon <zhaopku09@gmail.com>
2024-06-15 09:32:18 +0800

    Update and rename start_gmssl_tclp.ps1 to start_gmssl_tlcp.ps1

---
+++ cmake/start_gmssl_tlcp.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "bin/gmssl.exe" -ArgumentList "tlcp_server -port 4433 -cert tlcp_server_certs.pem -key signkey.pem -pass P@ssw0rd -ex_key enckey.pem -ex_pass P@ssw0rd" -NoNewWindow -RedirectStandardOutput "tlcp_server.log" -RedirectStandardError "tlcp_server.log"
