fae6b807501279a8664e0f6d4f697d28fc4f5a89
Simon <zhaopku09@gmail.com>
2024-06-15 12:28:51 +0800

    Update start_gmssl_tlcp_client.ps1

--- cmake/start_gmssl_tlcp_client.ps1
+++ cmake/start_gmssl_tlcp_client.ps1
@@ -1,6 +1,6 @@
 param (
-    [string]$gmsslPath = "..\\build\\bin\\gmssl.exe",
-    [string]$arguments = "tlcp_server -port 4433 -cert ..\\build\\tlcp_server_certs.pem -key ..\\build\\signkey.pem -pass P@ssw0rd -ex_key ..\\build\\enckey.pem -ex_pass P@ssw0rd",
+    [string]$gmsslPath = " D:/a/GmSSL/GmSSL/build/bin/gmssl.exe",
+    [string]$arguments = "tlcp_server -port 4433 -cert D:/a/GmSSL/GmSSL/build/tlcp_server_certs.pem -key D:/a/GmSSL/GmSSL/build/signkey.pem -pass P@ssw0rd -ex_key D:/a/GmSSL/GmSSL/build/enckey.pem -ex_pass P@ssw0rd",
     [string]$logFile = "tlcp_server.log"
 )
 
