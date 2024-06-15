7317518d2725bd2f40ff291939c57fe126a9064b
Simon <zhaopku09@gmail.com>
2024-06-15 13:34:04 +0800

    Update start_gmssl_tlcp.ps1

--- cmake/start_gmssl_tlcp.ps1
+++ cmake/start_gmssl_tlcp.ps1
@@ -1,20 +1,14 @@
-$process = Start-Process -FilePath "bin/gmssl.exe" `
-                         -ArgumentList "tlcp_server -port 4433 -cert tlcp_server_certs.pem -key signkey.pem -pass P@ssw0rd -ex_key enckey.pem -ex_pass P@ssw0rd" `
+param (
+    [string]$gmsslPath = " D:/a/GmSSL/GmSSL/build/bin/gmssl.exe",
+    [string]$arguments = "tlcp_server -port 4433 -cert D:/a/GmSSL/GmSSL/build/tlcp_server_certs.pem -key D:/a/GmSSL/GmSSL/build/signkey.pem -pass P@ssw0rd -ex_key D:/a/GmSSL/GmSSL/build/enckey.pem -ex_pass P@ssw0rd",
+    [string]$logFile = "tlcp_server.log"
+)
+
+# 启动进程并获取进程对象
+$process = Start-Process -FilePath $gmsslPath `
+                         -ArgumentList $arguments `
                          -NoNewWindow `
-                         -RedirectStandardOutput "tlcp_server.log" `
+                         #-RedirectStandardOutput $logFile `
                          -PassThru
-
-# 等待进程完成或超时
-$timeout = 3 # 设置超时时间，单位为秒
-$waitResult = $process.WaitForExit($timeout * 1000)  # 将超时时间转换为毫秒
-
-if (-not $waitResult) {
-    Write-Host "Process completed successfully."
-    exit 0  # 返回零值，表示成功
-} else {
-    Write-Host "Process did not complete within $timeout seconds. Terminating."
-    $process.Kill()  # 如果超时，终止进程
-    $process.WaitForExit()  # 等待进程完全退出
-    Write-Host "Process terminated due to timeout."
-    exit 1  # 返回一个非零值，表示超时错误
-}
+Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
+exit 0
