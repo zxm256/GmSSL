994ee946d898cc7cb7d72f96ea0faeec7c1f56a1
Simon <zhaopku09@gmail.com>
2024-06-15 14:04:23 +0800

    Update start_gmssl_tlcp.ps1

--- cmake/start_gmssl_tlcp.ps1
+++ cmake/start_gmssl_tlcp.ps1
@@ -1,13 +1,13 @@
-param (
-    [string]$gmsslPath = "D:\a\GmSSL\GmSSL\build\bin\gmssl.exe",
-    [string]$arguments = "--help"
-)
+#param (
+#    [string]$gmsslPath = "D:\a\GmSSL\GmSSL\build\bin\gmssl.exe",
+#    [string]$arguments = "--help"
+#)
 
 # 输出调试信息
-Write-Host "gmsslPath: $gmsslPath"
-Write-Host "arguments: $arguments"
-Write-Host "Current directory: $(Get-Location)"
-Write-Host "Files in directory: $(Get-ChildItem)"
+#Write-Host "gmsslPath: $gmsslPath"
+#Write-Host "arguments: $arguments"
+#Write-Host "Current directory: $(Get-Location)"
+#Write-Host "Files in directory: $(Get-ChildItem)"
 
 # 启动进程并获取进程对象
 #$process = Start-Process -FilePath "$gmsslPath" `
@@ -16,7 +16,7 @@ Write-Host "Files in directory: $(Get-ChildItem)"
 #                         #-RedirectStandardOutput $logFile `
 #                         -PassThru#
 
-Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
+#Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
 
 # 结束脚本
 exit 0
