9850d4a3dd792508dc1171bf8527a6f4dee468b2
Simon <zhaopku09@gmail.com>
2024-06-15 14:21:59 +0800

    Update start_gmssl_tlcp.ps1

--- cmake/start_gmssl_tlcp.ps1
+++ cmake/start_gmssl_tlcp.ps1
@@ -1,7 +1,7 @@
-#param (
-#    [string]$gmsslPath = "D:\a\GmSSL\GmSSL\build\bin\gmssl.exe",
-#    [string]$arguments = "--help"
-#)
+param (
+    [string]$gmsslPath = "D:\a\GmSSL\GmSSL\build\bin\gmssl.exe",
+    [string]$arguments = "-help"
+)
 
 # 输出调试信息
 #Write-Host "gmsslPath: $gmsslPath"
@@ -10,13 +10,13 @@
 #Write-Host "Files in directory: $(Get-ChildItem)"
 
 # 启动进程并获取进程对象
-#$process = Start-Process -FilePath "$gmsslPath" `
-#                         -ArgumentList $arguments `
-#                         -NoNewWindow `
-#                         #-RedirectStandardOutput $logFile `
-#                         -PassThru#
+$process = Start-Process -FilePath "$gmsslPath" `
+                         -ArgumentList $arguments `
+                         -NoNewWindow `
+                         #-RedirectStandardOutput $logFile `
+                         -PassThru#
 
-#Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
+Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
 
 # 结束脚本
 exit 0
