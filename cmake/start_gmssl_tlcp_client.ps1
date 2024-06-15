b1d53afad7931df6946ac0201b1df0c0f4115d3c
Simon <zhaopku09@gmail.com>
2024-06-15 13:23:27 +0800

    Update start_gmssl_tlcp_client.ps1

--- cmake/start_gmssl_tlcp_client.ps1
+++ cmake/start_gmssl_tlcp_client.ps1
@@ -1,21 +1,15 @@
 param (
-    [string]$gmsslPath = "D:\\a\\GmSSL\\GmSSL\\build\\bin\\gmssl.exe",
+    [string]$gmsslPath = "bin/gmssl.exe",
     [string]$arguments = "--help"
-   # [string]$arguments = "tlcp_server -port 4433 -cert D:/a/GmSSL/GmSSL/build/tlcp_server_certs.pem -key D:/a/GmSSL/GmSSL/build/signkey.pem -pass P@ssw0rd -ex_key D:/a/GmSSL/GmSSL/build/enckey.pem -ex_pass P@ssw0rd",
-  #  [string]$logFile = "tlcp_server.log"
+    #[string]$arguments = "tlcp_server -port 4433 -cert tlcp_server_certs.pem -key signkey.pem -pass P@ssw0rd -ex_key enckey.pem -ex_pass P@ssw0rd",
+    [string]$logFile = "tlcp_server.log"
 )
 
-try {
-    # 启动进程并获取进程对象
-    $process = Start-Process -FilePath $gmsslPath `
-                            -NoNewWindow `
-                            -ArgumentList $arguments `
-                            #-RedirectStandardOutput $logFile `
-                            -PassThru
-
-    Start-Sleep -Seconds 5
-    exit 0  # 正常退出
-} catch {
-    Write-Host "PowerShell script failed: $_"
-    exit 1  # 异常退出
-}
+# 启动进程并获取进程对象
+$process = Start-Process -FilePath $gmsslPath `
+                         -ArgumentList $arguments `
+                         -NoNewWindow `
+                         -RedirectStandardOutput $logFile `
+                         -PassThru
+Start-Sleep -Seconds 5  # 等待几秒钟以便服务器启动
+exit 0
